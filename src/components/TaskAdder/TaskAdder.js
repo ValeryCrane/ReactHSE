34c188f81c7707696ccf1ce11271b0ec88d6959e
Valery Crane <zhulery@gmail.com>
2020-12-11 02:31:01 +0500

    Added some comments

--- src/components/TaskAdder/TaskAdder.js
+++ src/components/TaskAdder/TaskAdder.js
@@ -1,53 +1,60 @@
-import React from 'react'
-import './TaskAdder.css'
+import React from "react";
+import "./TaskAdder.css";
 
-class TaskAdder extends React.Component{
+class TaskAdder extends React.Component {
 
+    //State, containing input values.
     state = {
-        name: '',
-        description: ''
-    }
+        name: "",
+        description: "",
+    };
 
-    handleNameChange = event => {
+    //Method, that handles change of the name input.
+    handleNameChange = (event) => {
         const { value } = event.target;
         this.setState({
             name: value,
-            description: this.state.description
-        })
-    }
+            description: this.state.description,
+        });
+    };
 
-    handleDescriptionChange = event => {
+    //Method, that handles change of the name input.
+    handleDescriptionChange = (event) => {
         const { value } = event.target;
         this.setState({
             name: this.state.name,
-            description: value
-        })
-    }
+            description: value,
+        });
+    };
 
+    //Method, that clears all inputs, after adding a task.
     addATask = () => {
-        if(!(this.state.description && this.state.name)) return;
+        if (!(this.state.description && this.state.name)) return;
         this.props.addATask(this.state.name, this.state.description);
         this.setState({
-            name: '',
-            description: ''
+            name: "",
+            description: "",
         });
-    }
+    };
 
-    render(){
-        return <div class="taskAdder">
-            <h1>Add a task</h1>
-            <input
-                value={this.state.name}
-                onChange={this.handleNameChange}
-                placeholder='name'
-            />
-            <input
-                value={this.state.description}
-                onChange={this.handleDescriptionChange}
-                placeholder='description'
-            />
-            <button onClick={this.addATask}>Add</button>
-        </div>
+    //Render, finally...
+    render() {
+        return (
+            <div class="taskAdder">
+                <h1>Add a task</h1>
+                <input
+                    value={this.state.name}
+                    onChange={this.handleNameChange}
+                    placeholder="name"
+                />
+                <input
+                    value={this.state.description}
+                    onChange={this.handleDescriptionChange}
+                    placeholder="description"
+                />
+                <button onClick={this.addATask}>Add</button>
+            </div>
+        );
     }
 }
 
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -3,6 +3,7 @@ import './TodoList.css';
 import Task from '../Task/Task.js';
 
 const Todolist = (props) => {
+    //Just rendering to-do list.
     return <div className="TodoList">
         {
             props.tasks.map(task => <Task task={task} key={task.id} click={props.click}/>)
